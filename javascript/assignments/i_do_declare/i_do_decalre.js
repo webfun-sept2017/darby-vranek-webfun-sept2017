a900e85574c20161dc34c448a19bb990c1e8943c
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-09-18 16:13:42 -0700

    i do declare completed

---
+++ javascript/assignments/i_do_declare/i_do_decalre.js
@@ -0,0 +1,23 @@
+var num0 = 0.5;
+console.log("Num0", num0);
+var num1 = 1512;
+console.log("Num1", num1);
+var num2 = -15.1;
+console.log("Num2", num2);
+var num3 = 14;
+console.log("Num3", num3);
+
+var string0 = "NCC-1701-D";
+console.log("String0", string0);
+var string1 = "natural lemon flavored sparkling water";
+console.log("String1", string1);
+var string2 = "Captain Jean-Luc Picard";
+console.log("String2", string2);
+
+var birdsAreCute = true;
+console.log("Birds are cute?", birdsAreCute);
+var birdsAreQuiet = false;
+console.log("Birds are quiet?", birdsAreQuiet);
+
+var notDefined;
+console.log("Undefined:", notDefined);
